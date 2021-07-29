2fbc3fa4f3d01cc72c55ff17c3e90622046c6c06
O Wooyoung <fz7948@gmail.com>
2021-07-29 14:07:42 +0900

    공통원소구하기

---
+++ inflearn/자바스크립트 알고리즘 풀이/5-효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/공통원소구하기.js
@@ -0,0 +1,35 @@
+// 인프런 - 공통 원소 구하기
+
+// solution 1
+function solution(arr1, arr2) {
+  let answer = [];
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  let p1 = 0;
+  let p2 = 0;
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1++]);
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) p1++;
+    else p2++;
+  }
+  return answer;
+}
+
+// solution 2
+function solution(arr1, arr2) {
+  let answer = [];
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1.length; j++) {
+      if (arr1[i] === arr2[j]) answer.push(arr1[i]);
+    }
+  }
+  return answer;
+}
+
+let a = [1, 3, 9, 5, 2];
+let b = [3, 2, 5, 7, 8];
+console.log(solution(a, b));
